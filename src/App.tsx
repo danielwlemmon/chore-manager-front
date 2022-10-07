af764e6f3378bb3d3217a3a7338913e5072d29fd
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-10-07 16:06:53 -0600

    route navigation setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import Login from "./components/login";
 import Nav from "./components/nav";
 export default function App() {
   return (
-    <div className="h-screen flex-grow flex justify-center">
-      <div className="flex-auto w-96">
-        <Nav />
+    <Router>
+      <div className="h-screen flex-grow flex justify-center">
+        <div className="flex-auto w-96">
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
